8a725ed8347b7af32ec298c91d1f53313118ae48
PRO X <bredalisgautreaux@gmail>
2024-05-29 11:31:14 +0200

    Modelo en proceso

---
+++ Modelo.py
@@ -0,0 +1,46 @@
+
+# Detector de Emociones en Música (Clasificacion)
+
+# Librerias
+
+
+
+# Obtener datos
+
+
+
+
+
+# Preparar los datos
+
+
+
+
+
+
+# Dividir los datos
+
+
+
+
+
+
+# Modelo
+
+
+
+
+
+
+# Entrenamiento y prediccion
+
+
+
+# Metricas
+
+
+
+
+
+
+
