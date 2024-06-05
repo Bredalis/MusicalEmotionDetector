af67245b7e170d2c8903cad25fa9c55a85eede3d
PRO X <bredalisgautreaux@gmail>
2024-06-05 12:45:15 +0200

    Modelo en proceso

--- Modelo.py
+++ Modelo.py
@@ -3,15 +3,28 @@
 
 # Librerias
 
+import pandas as pd
+
 
 
 # Obtener datos
 
-
-
-
+df = pd.read_excel("Songs.xlsx")
+df =  df.head(30)
 
 # Preparar los datos
+
+df = df.drop(["ID", "Names"], axis = 1)
+
+print("DF:", df)
+print(df.columns)
+
+for i in range(30):
+	print(df["Lyrics "][i].lower())
+
+print(df["Lyrics "])
+
+# One Hot Encoding
 
 
 
